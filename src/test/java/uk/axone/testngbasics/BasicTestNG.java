2b47605d0800446e586848c3a73ed71381e61cb2
Karthikakalaimani <karthikakalaimani10@gmail.com>
2022-05-11 23:04:32 +0100

    First Commit

---
+++ src/test/java/uk/axone/testngbasics/BasicTestNG.java
@@ -0,0 +1,13 @@
+package uk.axone.testngbasics;
+
+import org.testng.annotations.Test;
+
+public class BasicTestNG {
+    @Test
+    public void myFirstTest(){
+        System.out.println("My First TestCase in TestNG");
+
+    }
+
+
+}
